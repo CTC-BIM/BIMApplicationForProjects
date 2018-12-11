26e3759f5e7d826eab29d5868b392510acef7c30
nhantc <nhantc@coteccons.vn>
2018-12-11 09:06:42 +0700

    Add project files.

---
+++ BIMApplicationForProjects/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BIMApplicationForProjects.Startup))]
+namespace BIMApplicationForProjects
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
